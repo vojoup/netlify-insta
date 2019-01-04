f15f6cf0d28a996191e85082a982315f11f09703
Vojta Oupicky <voj.oup@gmail.com>
2019-01-04 20:03:16 +0100

    a

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,4 +1,4 @@
-export function handler(event, context, callback) {
+export.handler(event, context, callback) {
   console.log('queryStringParameters', event.queryStringParameters);
   callback(null, {
     statusCode: 200,
