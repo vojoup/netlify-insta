d9ee39fa898e237444c9aa66ee48fe19e295e703
Vojta Oupicky <voj.oup@gmail.com>
2019-01-04 21:05:03 +0100

    a

--- src/functions/instagram.js
+++ src/functions/instagram.js
@@ -1,12 +1,9 @@
-const axios = require('axios');
-
 exports.handler = function instagram(event, context, callback) {
   const endpoint = 'https://api.instagram.com/v1/users/self/media/recent';
   const token = process.env.INSTAGRAM_ACCESS_TOKEN;
   const limit = 5;
 
-  axios
-    .get(`${endpoint}?access_token=${token}&count=${limit}`)
+  fetch(`${endpoint}?access_token=${token}&count=${limit}`)
     .then(({ data: { data: posts } }) => {
       callback(null, {
         statusCode: 200,
