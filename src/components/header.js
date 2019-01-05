4c4b09c05f8dc5a26c79eecd1ae24b24b6845969
Vojta Oupicky <voj.oup@gmail.com>
2019-01-05 21:31:13 +0100

    Add netlify identity

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,42 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
-import React from 'react'
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+const netlifyIdentity = require('netlify-identity-widget');
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  componentDidMount() {
+    netlifyIdentity.init();
+  }
+
+  render() {
+    const { siteTitle } = this.props;
+    return (
+      <div className="header-wrapper">
+        <div className="header">
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+          <div data-netlify-identity-menu />
+        </div>
+      </div>
+    );
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
